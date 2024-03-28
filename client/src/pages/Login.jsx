dc19b1541b2ccc437a5b2d62a0f8c9808c51e754
Alexander Quesada <alexquesada22@gmail.com>
2024-03-28 10:13:44 -0600

    finished login structure

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,8 +4,12 @@ import '../styles.scss';
 const Login = () => {
   return (
     <div>
+      <div className='credentials-container'>
+        <a href='#login'>Loguearse</a>
+        <a href='#register'>Registrarse</a>
+      </div>
       <h2>
-      Un placer verte por acá
+        Un placer verte por acá
       </h2>
 
       <form id="contactForm">
@@ -13,7 +17,11 @@ const Login = () => {
         <input type="email" id="email" name="email" required />
 
         <label htmlFor="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required/>
+        <input type="password" id="password" name="password" required />
+        <button type="submit">Iniciar Sesión</button>
+        <a href="#pass_recover" className='forgot_pass'>Olvido la contraseña?</a>
+        <hr class="solid"></hr>
+        <a href="#register" className='no_account'>No ha creado una cuenta aún?</a>
       </form>
     </div>
   );
